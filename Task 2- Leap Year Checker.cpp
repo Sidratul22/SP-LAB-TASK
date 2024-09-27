38a27ad2f41982756d6ef20b7b5d0c2c6bac526a
Sidratul22 <sidratulmoontahar22@gmail.com>
2024-09-27 20:47:52 +0600

    Create Task 2- Leap Year Checker.cpp

---
+++ Task 2- Leap Year Checker.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+          int n;
+          printf("Enter a year ");
+          scanf("%d",&n);
+          if(n%400==0){
+                    printf("It is a leap year\n");
+          }
+          else if(n%100==0){
+                    printf("It is not a leap year\n");
+          }
+          else if(n%4==0){
+                    printf("It is a leap year\n");
+          }
+          else{
+                    printf("It is not a leap year\n");
+          }
+          return 0;
+}
