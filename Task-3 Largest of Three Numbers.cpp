e9c6fe14c7f3383b1d90a1e562b8486ece9e9a33
Sidraul027 <sidratulmoontahar22@gmail.com>
2024-09-28 11:13:35 +0600

    Update Task-3 Largest of Three Numbers.cpp

--- Task-3 Largest of Three Numbers.cpp
+++ Task-3 Largest of Three Numbers.cpp
@@ -2,7 +2,7 @@
 int main()
 {
           int a,b,c,max;
-          printf("Enter ant three numbers ");
+          printf("Enter any three numbers ");
           scanf("%d %d %d",&a,&b,&c);
           if(a>=b)
           {    if(a>=c)
