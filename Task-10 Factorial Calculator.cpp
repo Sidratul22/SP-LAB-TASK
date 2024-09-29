a83fdc5acc4cfd2bb57037719df6141cae933eb1
Sidraul027 <sidratulmoontahar22@gmail.com>
2024-09-29 11:55:22 +0600

    Create Task-10 Factorial Calculator.cpp

---
+++ Task-10 Factorial Calculator.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n,fact=1;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    int i=1;
+    while(i<=n){
+        fact*=i;
+        i++;
+    }
+    printf("Factorial= %d\n",fact);
+    return 0;
+}
