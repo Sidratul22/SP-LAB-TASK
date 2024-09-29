fa9f3b3251f5df5e762b5bb548e5a91a9b6a4e9b
Sidraul027 <sidratulmoontahar22@gmail.com>
2024-09-29 12:16:18 +0600

    Task-8 Triangle Validity Checker.cpp

---
+++ Task-8 Triangle Validity Checker.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+          float a,b,c;
+          printf("Enter three lengths ");
+          scanf("%f %f %f",&a,&b,&c);
+          if(b+c>a && a+c>b && a+b>c)
+          {
+           printf("It is a triangle");
+          }
+          else{printf("It is not a triangle");}
+          return 0;
+}
